48a3da0bb17a9baaf9287db259c3c0b594bc42de
jdpieck <143460398+jdpieck@users.noreply.github.com>
2026-01-20 13:08:17 -0800

    cleared old unused files

--- CycloneRobsubGUI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CycloneRobsubGUI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
