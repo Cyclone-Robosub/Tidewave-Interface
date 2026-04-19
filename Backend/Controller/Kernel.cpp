0414c8e855e574e0de92abc17310a1a29abf8807
Tanishq Dwivedi <tdwivedi@ucdavis.edu>
2026-04-19 15:38:04 -0700

    fixing backend mistakes

--- Backend/Controller/Kernel.cpp
+++ Backend/Controller/Kernel.cpp
@@ -1,13 +1,14 @@
 #include "../Model.hpp"
+#include <iostream>
 #include <shared_mutex>
-std::condition_varaible KernelMessenger;
-void StartThread(){
+std::condition_variable KernelMessenger;
+void StartThread(std::shared_ptr<DataModel> dataModel){
 //INITALIZE All	Inital Threads for Kernel
 //Wait for Start
 	//Wait for Permission to Check
 		std::shared_lock<std::shared_mutex> lk(dataModel->control_path.ControlDataMutex);
 	//Wait until we get the signal to Start
-		dataModel->control_path.Messenger.wait(lk,[]{return dataModel->control_path.isRunning});
+		dataModel->control_path.Messenger.wait(lk,[&]{return dataModel->control_path.isRunning;});
     std::cout << "------------STARTING ROBOT KERNEL--------------\n";
 //Execute Start -> NOTIFY ANY OTHER KERNEL THREADS
 		KernelMessenger.notify_all();
